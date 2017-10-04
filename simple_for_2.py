3ff4a4208a3f97008dce8f60a02a584981069261
Rafael Santos <rafaels@gmail.com>
2017-10-03 21:13:35 -0400

    increment for loop with enumerate function

--- simple_for_2.py
+++ simple_for_2.py
@@ -2,3 +2,8 @@ surnames = ['Murray', 'Peters', 'Fenelain']
 
 for position in range(len(surnames)):
     print(position, surnames[position])
+
+print('#' * 10)
+
+for position, surname in enumerate(surnames):
+    print(position, surname)
